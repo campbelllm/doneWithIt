80b024a2bddcdd3eab4e0a08ab3be2be8d9435a3
campbelllm <campbelllm@gmail.com>
2023-03-09 16:02:30 -0800

    add jsconfig and app.js changes

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+ import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Platform, Text, StatusBar, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  return ( 
+    <SafeAreaView style={styles.container}>
+      <Text>hello</Text>
+      <Image></Image>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +14,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight
   },
 });
